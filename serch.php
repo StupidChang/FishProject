273cacecc0b155685fa8e822f49f44c89265a09d
ccc <f0806854@gmail.com>
2023-02-10 00:00:57 +0800

    values that in database will push to serch.json

--- serch.php
+++ serch.php
@@ -4,15 +4,20 @@
     if($conn->connect_error){
         die("Connetion failed: ". $conn->connect_error);
     }
-    $key = false;
+    $table_key = false;
     $sql = "SELECT * FROM ph";
     $result = $conn->query($sql);
-    if($result ->num_rows > 0){
+    if($result -> num_rows > 0){
         while($row = $result->fetch_assoc()){
+            $data[] = array(
+                "ph"=>$row["value"],
+            );
             
         }
-        if(!$key){
-            //create($conn);
-        }
-    }
+        
+    }  
+    $json = json_encode($data);
+    
+    file_put_contents("serch.json",$json);
+    $conn->close();
 ?>
